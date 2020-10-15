cce29dbb230bb3f57bdcb5e33e3c23d4266ac9b0
Mohamed <mhm@umn.edu>
2020-10-15 16:43:46 -0500

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import {GatsbySeo} from "gatsby-plugin-next-seo";
+import { Link } from "gatsby"
 
 export default () => (
     <>
@@ -8,10 +9,17 @@ export default () => (
             title="Page Not Found"
             titleTemplate={`%s | KerryJo Felder 4 Minneapolis School Board: District 2`}
         />
-  <Layout>
-    <div>
-      <h1>Oops, that's a 404</h1>
-    </div>
-  </Layout>
+        <Layout bodyClass="page-template-default page page-id-home wp-embed-responsive singular missing-post-thumbnail has-no-pagination not-showing-comments footer-top-visible customize-support">
+            <article
+                className="post-home post page type-page status-publish hentry"
+                id="post-home"
+            >
+
+                <div className="post-inner thin pf">
+                    <h1>Oops, that's a 404</h1>
+                    <h5><Link to="/">Click here to go back to the homepage</Link></h5>
+                </div>
+            </article>
+        </Layout>
         </>
 )
